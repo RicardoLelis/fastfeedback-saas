25e88c991649abee3cfeca46b27c3ffc885cfeb6
rlelis <ricardo.lelis3@gmail.com>
2021-04-10 21:43:45 +0100

    Update UI ep03 (#2)
    * Added chakra-ui as UI template. Added user authentication with firebase with github as provider. Added also the user database with firestore. The index.js was streamlined to fit our project
    
    * Adding UI with chakra-ui. Added Firebase auth and Firestore sites creation.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { Global, css } from "@emotion/core";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { AuthProvider } from "@/lib/auth";
+import customTheme from "@/styles/theme";
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width:360px;
+            scroll-behavior: smooth;
+          }
+          
+          #__next {
+            display:flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  )
 }
 
-export default MyApp
+const App = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={customTheme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+
+    </ThemeProvider>
+  )
+};
+
+export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,36 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Button, Flex, Text, Code, Icon } from "@chakra-ui/core"
 
-export default function Home() {
+import { useAuth } from "@/lib/auth";
+import EmptyState from '@/components/EmptyState';
+
+
+const Home = () => {
+  const auth = useAuth()
+
   return (
-    <div className={styles.container}>
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+    >
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Fast Feedback</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Fast feedback!</h1>
+      <Icon color="black" name="logo" size="64px" ></Icon>
+      {auth.user ? (
+        <EmptyState />
+      ) : (
+        // <Button onClick={(e) => auth.signout()}>Sign Out</Button>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+        <Button mt={4} size="sm"
+          onClick={(e) => auth.signinWithGithub()}>Sign In</Button>
+      )}
+    </Flex>
   )
 }
+
+export default Home;
